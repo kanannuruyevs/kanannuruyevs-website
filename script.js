05343658bc4a0087bafab7a71586cd922b815245
Kanan N <kanansnote@gmail.com>
2023-10-12 15:34:00 +0400

    Implemented the correct adjustments the nav menu and social links in the burger menu itself.

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 // JavaScript to toggle the burger menu on and off
 const burgerMenu = document.querySelector('.burger-menu');
-const closeMenu = document.querySelector('.burger-menu-close'); // select the close button
 const navMenu = document.querySelector('.nav-menu');
 const socialLinks = document.querySelector('.social-links'); // select the social links
 
 burgerMenu.addEventListener('click', () => {
-    navMenu.style.display = "block";
-    socialLinks.style.display = "block"; // show the social links
-    closeMenu.style.display = "block"; // show the close button
-});
-
-closeMenu.addEventListener('click', () => {
-    navMenu.style.display = "none";
-    socialLinks.style.display = "none"; // hide the social links
-    closeMenu.style.display = "none"; // hide the close button
+    navMenu.classList.toggle('show');
+    socialLinks.classList.toggle('show'); // toggle the social links
 });
