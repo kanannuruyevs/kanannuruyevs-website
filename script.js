e7f049372e6f150ff1e6a89df47acb7c3ff51458
Kanan N <kanansnote@gmail.com>
2023-10-12 12:18:40 +0400

    Minor work on the social links class elements.

--- script.js
+++ script.js
@@ -1,45 +1,17 @@
-// JavaScript to toggle the burger menu and social links on and off
+// JavaScript to toggle the burger menu on and off
 const burgerMenu = document.querySelector('.burger-menu');
+const closeMenu = document.querySelector('.burger-menu-close'); // select the close button
 const navMenu = document.querySelector('.nav-menu');
 const socialLinks = document.querySelector('.social-links'); // select the social links
 
 burgerMenu.addEventListener('click', () => {
-    if (navMenu.style.display === "none") {
-        navMenu.style.display = "block";
-        socialLinks.style.display = "block"; // show the social links
-    } else {
-        navMenu.style.display = "none";
-        socialLinks.style.display = "none"; // hide the social links
-    }
-});
-
-// Optional: Close the menu and social links when a link is clicked
-const navLinks = document.querySelectorAll('.nav-menu ul li a');
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navMenu.style.display = "none";
-        socialLinks.style.display = "none"; // hide the social links
-    });
-});
-
-// Initialize Hammer.js on the burger menu element
-const hammer = new Hammer(burgerMenu);
-
-// Detect swipe right gesture to open the mobile menu and social links
-hammer.on('swiperight', function(event) {
     navMenu.style.display = "block";
     socialLinks.style.display = "block"; // show the social links
+    closeMenu.style.display = "block"; // show the close button
 });
-
-// Detect swipe left gesture to close the mobile menu and social links
-hammer.on('swipeleft', function(event) {
-    navMenu.style.display = "none";
-    socialLinks.style.display = "none"; // hide the social links
-});
-
-const closeMenu = document.querySelector('.burger-menu-close'); // select the close button
 
 closeMenu.addEventListener('click', () => {
     navMenu.style.display = "none";
-    socialLinks.style.display = "none";
+    socialLinks.style.display = "none"; // hide the social links
+    closeMenu.style.display = "none"; // hide the close button
 });
