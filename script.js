cd376bf4b073bac57903c77ae8f630d462c1660f
kanan <kanansnote@gmail.com>
2023-10-29 16:14:54 +0400

    Added website icon to title and banner by adjusting its position on desktop and mobile.

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ burgerMenu.addEventListener('click', () => {
     socialLinks.classList.toggle('show'); // toggle the social links
     burgerMenu.classList.toggle('open'); // toggle the open class on burger menu
 });
+
+window.addEventListener('load', function() {
+  var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/jpeg';
+  link.rel = 'icon';
+  link.href = 'spiderswebflat.jpeg'; // replace with your favicon path
+  document.getElementsByTagName('head')[0].appendChild(link);
+});
