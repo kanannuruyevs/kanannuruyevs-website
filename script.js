28a9db7c0789eeafd005fb3e973aab3773219aa3
kanan <kanansnote@gmail.com>
2023-12-22 11:17:15 +0400

    Significant changes: updates to the website's page structure, new upcoming blog content, and more.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// JavaScript to toggle the burger menu on and off
+// Toggle the burger menu on and off
 const burgerMenu = document.querySelector('.burger-menu');
 const navMenu = document.querySelector('.nav-menu');
 const socialLinks = document.querySelector('.social-links'); // select the social links
@@ -7,4 +7,17 @@ burgerMenu.addEventListener('click', () => {
     navMenu.classList.toggle('show');
     socialLinks.classList.toggle('show'); // toggle the social links
     burgerMenu.classList.toggle('open'); // toggle the open class on burger menu
+});
+
+// Left and right arrow buttons for scrolling through projects
+const scrollContainer = document.querySelector('.scrollContainer');
+const arrowLeft = document.getElementById('arrowLeftButton');
+const arrowRight = document.getElementById('arrowRightButton');
+
+arrowLeft.addEventListener('click', () => {
+  scrollContainer.scrollLeft -= 200; // Change this value to adjust scroll distance
+});
+
+arrowRight.addEventListener('click', () => {
+  scrollContainer.scrollLeft += 200; // Change this value to adjust scroll distance
 });
